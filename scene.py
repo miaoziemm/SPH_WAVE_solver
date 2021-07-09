b3d8793800997678b10a19132b6a205c6a6c3a44
miaoziemm <changzm2316@126.com>
2021-07-09 11:13:46 +0800

    主要运行代码main_code

---
+++ scene.py
@@ -0,0 +1,112 @@
+# SPH taichi implementation by mzhang
+import taichi as ti
+from engine.sph_solver import *
+import argparse
+
+# Default run on CPU
+# cuda performance has not been tested
+ti.init(arch=ti.cpu)
+
+
+def main():
+    dynamic_allocate = False
+    save_frames = True
+    adaptive_time_step = True
+    method_name = 'WCSPH'
+    #method_name = 'PCISPH'
+    # method_name = 'DFSPH'
+
+    sim_physical_time = 5.0
+    max_frame = 50000
+
+    res = (400, 400)
+    screen_to_world_ratio = 35
+    dx = 0.1
+    u, b, l, r = np.array([res[1], 0, 0, res[0]]) / screen_to_world_ratio
+
+    gui = ti.GUI('SPH', res, background_color=0x112F41)
+    sph = SPHSolver(res,
+                    screen_to_world_ratio, [u, b, l, r],
+                    alpha=0.30,
+                    dx=dx,
+                    max_time=sim_physical_time,
+                    max_steps=max_frame,
+                    dynamic_allocate=dynamic_allocate,
+                    adaptive_time_step=adaptive_time_step,
+                    method=SPHSolver.methods[method_name])
+
+    print("Method use: %s" % method_name)
+    # Add fluid particles
+    sph.add_cube(lower_corner=[res[0] / 2 / screen_to_world_ratio - 3, 4 * dx],
+                 cube_size=[6, 6],
+                 velocity=[0.0, -5.0],
+                 density=[1000],
+                 color=0x068587,
+                 material=SPHSolver.material_fluid)
+
+    colors = np.array([0xED553B, 0x068587, 0xEEEEF0, 0xFFFF00],
+                      dtype=np.uint32)
+    add_cnt = 0.0
+    add = True
+    save_cnt = 0.0
+    output_fps = 60
+    save_point = 1.0 / output_fps
+    t = 0.0
+    frame = 0
+    total_start = time.process_time()
+    while frame < max_frame and t < sim_physical_time:
+        dt = sph.step(frame, t, total_start)
+        particles = sph.particle_info()
+
+        # if frame == 1000:
+        if add and add_cnt > 0.40:
+            sph.add_cube(lower_corner=[6, 6],
+                         cube_size=[2.0, 2.0],
+                         velocity=[0.0, -5.0],
+                         density=[1000.0],
+                         color=0xED553B,
+                         material=SPHSolver.material_fluid)
+
+        # if frame == 1000:
+        if add and add_cnt > 0.40:
+            sph.add_cube(lower_corner=[3, 8],
+                         cube_size=[1.0, 1.0],
+                         velocity=[0.0, -10.0],
+                         density=[1000.0],
+                         color=0xEEEEF0,
+                         material=SPHSolver.material_fluid)
+            add = False
+
+        for pos in particles['position']:
+            for j in range(len(res)):
+                pos[j] *= screen_to_world_ratio / res[j]
+
+        gui.circles(particles['position'],
+                    radius=1.5,
+                    color=particles['color'])
+
+        # Save in fixed frame interval, for fixed time step
+        if not adaptive_time_step and frame % 50 == 0:
+            gui.show(f'{frame:06d}.png' if save_frames else None)
+
+        # Save in fixed frame per second, for adaptive time step
+        if adaptive_time_step and save_cnt >= save_point:
+            gui.show(f'{frame:06d}.png' if save_frames else None)
+            save_cnt = 0.0
+
+        frame += 1
+        t += dt
+        save_cnt += dt
+        add_cnt += dt
+
+    print('done')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--method",
+                        type=str,
+                        default="PCISPH",
+                        help="SPH methods: WCSPH, PCISPH, DFSPH")
+    opt = parser.parse_args()
+    main()
